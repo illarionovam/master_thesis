61f08f3ff00b3f49d3f05dc7b90dcdfe51358fd3
Mariia Illarionova <illarionovam@gmail.com>
2025-10-09 15:04:41 +0300

    base commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # master_thesis
+
+~/Documents/GoIT/projects/master_thesis/app/api$ npm init -y
+~/Documents/GoIT/projects/master_thesis/app/api$ npm install express
+~/Documents/GoIT/projects/master_thesis/app/api$ npm install dotenv
+
+node server.js
---
+++ app/api/server.js
@@ -0,0 +1,13 @@
+import 'dotenv/config';
+import express from 'express';
+
+const app = express();
+const SERVER_PORT = process.env.SERVER_PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(SERVER_PORT, () => {
+    console.log(`Server is running on port ${SERVER_PORT}`);
+});
