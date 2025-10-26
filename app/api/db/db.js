2d985abec4b7611e5742c2d100faa9b8a967b5ce
Mariia Illarionova <illarionovam@gmail.com>
2025-10-26 22:02:05 +0200

    pg + esm

--- app/api/db/db.js
+++ app/api/db/db.js
@@ -1,3 +1,4 @@
+import pg from 'pg';
 import { Sequelize } from 'sequelize';
 
 const { DB_HOST, DB_NAME, DB_USER, DB_PASSWORD } = process.env;
@@ -8,6 +9,8 @@ export const sequelize = new Sequelize({
     password: DB_PASSWORD,
     database: DB_NAME,
     dialectOptions: {
+        dialect: 'postgres',
+        dialectModule: pg,
         ssl: {
             require: true,
             rejectUnauthorized: false,
