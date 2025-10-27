303b8ecfbc3d4e8e4d9c69d0184d1b97d1f8f882
Mariia Illarionova <illarionovam@gmail.com>
2025-10-27 15:47:34 +0200

    added all methods to auth api

--- app/web/src/api/auth.js
+++ app/web/src/api/auth.js
@@ -2,14 +2,21 @@ import axiosInstance from './defaults';
 import axiosInstanceWithSpecialScope from './specialScope';
 
 export const signUpAppUser = async payload => await axiosInstance.post('/auth/sign-up', payload);
+
 export const signInAppUser = async payload => await axiosInstance.post('/auth/sign-in', payload);
+
+export const resetAppUserPassword = async payload => await axiosInstance.post('/auth/forgot-password', payload);
+
+export const getAppUserInfo = async () => await axiosInstance.get('/auth/user-info');
+
+export const signOutAppUser = async payload => await axiosInstance.post('/auth/sign-out', payload);
+
+export const updateAppUser = async payload => await axiosInstance.post('/auth/update', payload);
+
+export const updateAppUserEmail = async payload => await axiosInstance.post('/auth/update-email', payload);
 
 export const confirmAppUserEmail = async token =>
     await axiosInstanceWithSpecialScope(token).post('/auth/confirm-email', {});
 
 export const confirmAppUserPassword = async (token, new_password) =>
     await axiosInstanceWithSpecialScope(token).post('/auth/confirm-password', { new_password });
-
-export const signOutAppUser = async payload => await axiosInstance.post('/auth/sign-out', payload);
-
-export const getAppUserInfo = async () => await axiosInstance.get('/auth/user-info');
